24f456f87fcb76c970096ecee33039e93ee0a444
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-12-28 18:10:55 -0800

    Update to add some message when field is empty

--- app/src/main/java/com/savannahostrowski/cricketchirpstemperature/MainActivity.java
+++ app/src/main/java/com/savannahostrowski/cricketchirpstemperature/MainActivity.java
@@ -6,14 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSubmit;
     EditText etNumberOfChirps;
     TextView tvResult;
-
-
+    DecimalFormat formatter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         tvResult.setVisibility(View.INVISIBLE);
 
+        formatter = new DecimalFormat("#0.0");
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int numChirps = Integer.parseInt(etNumberOfChirps.getText().toString().trim());
 
-                float temperature = (numChirps / 3) + 4;
-                String text = "The temperature is: " + temperature;
-                tvResult.setText(text);
-                tvResult.setVisibility(View.VISIBLE);
+                if (etNumberOfChirps.getText().toString().isEmpty()) {
+                    tvResult.setVisibility(View.INVISIBLE);
+                    Toast.makeText(MainActivity.this, "Please enter a number", Toast.LENGTH_SHORT).show();
+                } else {
+                    int numChirps = Integer.parseInt(etNumberOfChirps.getText().toString().trim());
+
+                    double temperature = (numChirps / 3.0) + 4;
+                    String text = "The approximate temperature is: " + temperature;
+                    tvResult.setText(text);
+                    tvResult.setVisibility(View.VISIBLE);
+                }
             }
         });
-
-
 
     }
 }
